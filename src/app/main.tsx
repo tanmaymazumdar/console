01c036451c5fc9c0dfdec51abe35ea1c30c23589
Dazum Dev <dazum.dev@gmail.com>
2026-06-04 02:46:10 +0530

    feat: redux provider

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,19 @@
 import { startTransition } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router/dom'
 
 import { router } from './routes'
+import { store } from './store'
 
 import './styles/main.scss'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 startTransition(() => {
-  root.render(<RouterProvider router={router} />)
+  root.render(
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 })
