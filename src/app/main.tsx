3a2e29ada9abd227b17ce4874f701d3773817e8d
Dazum Dev <dazum.dev@gmail.com>
2026-06-04 01:05:01 +0530

    feat: sass framework and react router

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,13 @@
 import { startTransition } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router/dom'
+
+import { router } from './routes'
+
+import './styles/main.scss'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 startTransition(() => {
-  root.render(<h1>Hello, World!</h1>)
+  root.render(<RouterProvider router={router} />)
 })
