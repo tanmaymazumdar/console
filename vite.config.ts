fc8f3508567c2be6919a48fe65b6b9b8c956f54c
Dazum Dev <dazum.dev@gmail.com>
2026-06-05 00:38:53 +0530

    fix: update vite server proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000/api',
+        target: 'http://localhost:4000/',
         changeOrigin: true
       }
     }
